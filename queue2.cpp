cd93139ff0c984027fdc849152f63ea30781f89f
availrum <154116417+availrum@users.noreply.github.com>
2024-04-11 05:41:21 +0900

    Add files via upload

---
+++ queue2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<map>
+#include<queue>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,b;
+    string a;
+    queue<int> st;
+    cin>>N;
+    for(int i=0; i<N; i++){
+        cin>>a;
+        if(a=="push"){
+            cin>>b;
+            st.push(b);
+        }
+        if(a=="pop"){
+            if(st.empty()==1){
+                cout<<"-1"<<'\n';
+                continue;
+            }
+            cout<<st.front()<<'\n';
+            st.pop();
+        }
+        if(a=="size"){
+            cout<<st.size()<<'\n';
+        }
+        if(a=="empty"){
+            cout<<int(st.empty())<<'\n';
+        }
+        if(a=="front"){
+            if(st.empty()==1){
+                cout<<"-1"<<'\n';
+                continue;
+            }
+            cout<<st.front()<<'\n';
+        }
+        if(a=="back"){
+            if(st.empty()==1){
+                cout<<"-1"<<'\n';
+                continue;
+            }
+            cout<<st.back()<<'\n';
+        }
+    }
+
+    return 0;
+}
