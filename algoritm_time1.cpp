b4b48c92fbe9f081528ad6321b19bd3e0e92210e
availrum <154116417+availrum@users.noreply.github.com>
2024-04-25 05:18:56 +0900

    Add files via upload

---
+++ algoritm_time1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//24262
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,tim;
+    cin>>n;
+    cout<<"1"<<'\n';
+    cout<<"0"<<'\n';
+    return 0;
+}
