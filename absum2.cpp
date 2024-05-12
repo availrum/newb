487d528b2477be04056384d166e5cc19bb4b0c6e
availrum <154116417+availrum@users.noreply.github.com>
2024-05-12 23:42:23 +0900

    Add files via upload

---
+++ absum2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//2558
+    ios::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // cout.tie(NULL);
+    int a,b;
+    int mon;
+    cin>>a>>b;
+    cout<<a+b<<'\n';
+
+}
