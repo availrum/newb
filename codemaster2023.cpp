19f071a74937eed6e5ba55281ff0c2bfdc74c927
availrum <154116417+availrum@users.noreply.github.com>
2024-06-21 12:00:00 +0900

    Add files via upload

---
+++ codemaster2023.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//28235
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string st;
+    cin>>st;
+    if(st=="SONGDO") cout<<"HIGHSCHOOL"<<'\n';
+    else if(st=="CODE") cout<<"MASTER"<<'\n';
+    else if(st=="2023") cout<<"0611"<<'\n';
+    else if(st=="ALGORITHM") cout<<"CONTEST"<<'\n';
+}
