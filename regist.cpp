7e67db47ab465263a2cfadddac920efb14b01a4e
availrum <154116417+availrum@users.noreply.github.com>
2024-07-19 05:48:27 +0900

    Add files via upload

---
+++ regist.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){//7287
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"235"<<'\n';
+    cout<<"najojoda"<<'\n';
+}
