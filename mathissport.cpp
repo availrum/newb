c12c37c43ba67ead675a374c4af6034c156fa739
availrum <154116417+availrum@users.noreply.github.com>
2024-08-21 02:53:39 +0900

    Add files via upload

---
+++ mathissport.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//15894
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    //1-4 2-8 3-12
+    N*=4;
+    cout<<N<<'\n';
+}
