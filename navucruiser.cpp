1e19405163f6422c1bdd6692116aa680c2c16d86
availrum <154116417+availrum@users.noreply.github.com>
2024-08-29 03:55:40 +0900

    Add files via upload

---
+++ navucruiser.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){//9654
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         \n";
+}
