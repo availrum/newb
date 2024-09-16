cb583d8e49fd6ce97c6427214e16462f7496eab2
availrum <154116417+availrum@users.noreply.github.com>
2024-09-17 05:13:49 +0900

    Add files via upload

---
+++ algoritm_time5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//24266
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long int n,num;
+    cin>>n;
+    num=n*n*n;
+    cout<<num<<'\n';
+    cout<<"3"<<'\n';
+}
