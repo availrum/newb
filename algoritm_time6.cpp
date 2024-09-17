4873de6b4f430a134e73057a73bcce244d744bba
availrum <154116417+availrum@users.noreply.github.com>
2024-09-18 05:36:31 +0900

    Add files via upload

---
+++ algoritm_time6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){//24266
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long int n,num;
+    cin>>n;
+    //7654/4321
+    num=n*(n-1)*(n-2)/6;
+    cout<<num<<'\n';
+    cout<<"3"<<'\n';
+}
