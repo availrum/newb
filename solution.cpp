0552f71af7375f6c3a0653843c9fab343a246b6c
availrum <154116417+availrum@users.noreply.github.com>
2024-09-23 01:59:00 +0900

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){//2467
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    int arr[N]={0,};
+    for(int i=0; i<N; ++i) cin>>arr[i];
+    int st=0, ed=N-1;
+    int mini1=arr[st], mini2=arr[ed];
+    int sum=mini1+mini2;
+    long int val=abs(sum);
+    while(st<ed){
+        sum=arr[st]+arr[ed];
+        if(val>abs(sum)){
+            val=abs(sum);
+            mini1=arr[st];
+            mini2=arr[ed];
+        }
+        if(sum>0){
+            --ed;
+        }
+        else if(sum<0){
+            ++st;
+        }
+        else{
+            break;
+        }
+    }
+    cout<<mini1<<' '<<mini2<<'\n';
+}
