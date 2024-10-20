5624dae4fc8015d4fab4e3758ea3c3c0ebe2f8fc
availrum <154116417+availrum@users.noreply.github.com>
2024-10-21 03:55:09 +0900

    Add files via upload

---
+++ hook.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){//10189
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #\n";
+}
