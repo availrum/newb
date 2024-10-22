f0ef7d0908d7171aae29a16b2a5e58534f8d41d4
availrum <154116417+availrum@users.noreply.github.com>
2024-10-23 03:29:17 +0900

    Add files via upload

---
+++ callcentor.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){//5339
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |"<<'\n';
+}
