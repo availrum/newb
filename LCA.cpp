11b4ca61a4be47d53647ded6c216366a2ac80b65
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2024-12-30 02:14:39 +0900

    Add files via upload

--- LCA.cpp
+++ LCA.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 struct node{
     int parent;
     int height;
 };
-int check(int N, node* tnode){
-    if(tnode[N].parent==N) return tnode[N].height=1;
-    else if(tnode[N].height) return tnode[N].height;
-    return tnode[N].height=check(tnode[N].parent, tnode)+1;
+vector<int> vec[50001];
+void dfs(int N, node* tnode, int cnt){
+    tnode[N].height=cnt;
+    for(int i=0; i<vec[N].size(); ++i){
+        if(!tnode[vec[N][i]].height){
+            tnode[vec[N][i]].parent=N;
+            dfs(vec[N][i], tnode, cnt+1);
+        }
+    }
 }
 int find(int a, int b, node* tnode){
     // cout<<'\n';
@@ -28,30 +34,28 @@ int find(int a, int b, node* tnode){
     }
     return a;
 }
-int main(){//3584
+int main(){//11437
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int T;
-    cin>>T;
-    for(int tcase=0; tcase<T; ++tcase){
-        int N;
-        cin>>N;
-        node tree[N+1];
-        for(int i=0; i<=N; ++i){
-            tree[i].parent=i;
-            tree[i].height=0;
-        }
-        int A,B;
-        for(int i=0; i<N-1; ++i){
-            cin>>A>>B;
-            tree[B].parent=A;
-        }
-        for(int i=1; i<=N; ++i){
-            check(i, tree);
-        }
-        // for(int i=1; i<=N; ++i) cout<<i<<' '<<tree[i].height<<'\n';
-        // cout<<'\n'<<"123"<<'\n'<<'\n';
+    int N;
+    cin>>N;
+    node tree[N+1];
+    for(int i=0; i<=N; ++i){
+        tree[i].parent=i;
+        tree[i].height=0;
+    }
+    int A,B;
+    for(int i=0; i<N-1; ++i){
+        cin>>A>>B;
+        vec[A].push_back(B);
+        vec[B].push_back(A);
+    }
+    dfs(1, tree, 1);
+    // cout<<'\n'<<"123"<<'\n'<<'\n';
+    int M;
+    cin>>M;
+    for(int i=0; i<M; ++i){
         int n1, n2;
         cin>>n1>>n2;
         int sol=find(n1, n2, tree);
