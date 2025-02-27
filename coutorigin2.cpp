2831a8d0b266d079ff9165d76827c685ad9a6b76
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-02-28 04:39:03 +0900

    Add files via upload

---
+++ coutorigin2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){//11719
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string st;
+    while(getline(cin,st)){
+        cout<<st<<'\n';
+        // if(st=="00") break;
+    }
+}
