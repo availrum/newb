2473779c7ef0c3e9c3986314d2ce9bfef0c96458
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-04-26 01:27:55 +0900

    Add files via upload

---
+++ puttingupgamepiece.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){//31995
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,M;
+    cin>>N>>M;
+    cout<<(N-1)*(M-1)*2<<'\n';
+}
