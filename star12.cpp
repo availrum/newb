79ad01d9eb576c8dc1641cb8717e1e3aacafb251
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-07-05 01:15:49 +0900

    Add files via upload

---
+++ star12.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){ //2522
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    for(int i=1; i<=N; ++i){
+        for(int j=0; j<N-i; ++j){
+            cout<<' ';
+        }
+        for(int j=0; j<i; ++j){
+            cout<<'*';
+        }
+        cout<<'\n';
+    }
+    for(int i=N-1; i>=1; --i){
+        for(int j=0; j<N-i; ++j){
+            cout<<' ';
+        }
+        for(int j=0; j<i; ++j){
+            cout<<'*';
+        }
+        cout<<'\n';
+    }
+}
