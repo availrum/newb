ac2ef1db9cdea54eea6df0fd7de883b7b8024e63
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-08-03 05:04:24 +0900

    Add files via upload

---
+++ palindrom_chukhwabi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){ //20944
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    string st="";
+    for(int i=0; i<N; ++i){
+        st+='a';
+    }
+    cout<<st<<'\n';
+} 
