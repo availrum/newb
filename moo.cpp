0166eac6adc73484c01ab41bdd6082c3f184da92
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-08-14 17:32:27 +0900

    Add files via upload

---
+++ moo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){ //1809
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"(___)"<<'\n';
+    cout<<"(o o)____/"<<'\n';
+    cout<<" @@      \\"<<'\n';
+    cout<<"  \\ ____,/"<<'\n';
+    cout<<"  //   //"<<'\n';
+    cout<<" ^^   ^^"<<'\n';
+} 
