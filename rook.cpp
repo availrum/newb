1cc362f221bc44f9a677f95b40da3d535bd82c01
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-08-16 03:28:46 +0900

    Add files via upload

---
+++ rook.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){ //2393
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"  ___  ___  ___"<<'\n';
+    cout<<"  | |__| |__| |"<<'\n';
+    cout<<"  |           |"<<'\n';
+    cout<<"   \\_________/"<<'\n';
+    cout<<"    \\_______/"<<'\n';
+    cout<<"     |     |"<<'\n';
+    cout<<"     |     |"<<'\n';
+    cout<<"     |     |"<<'\n';
+    cout<<"     |     |"<<'\n';
+    cout<<"     |_____|"<<'\n';
+    cout<<"  __/       \\__"<<'\n';
+    cout<<" /             \\"<<'\n';
+    cout<<"/_______________\\"<<'\n';
+}
