aba9b50a5be65a16e2a9cb31f702f5eb7f756ae3
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-10-20 00:20:21 +0900

    Add files via upload

---
+++ chocolate_cutting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){ //2163
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,M;
+    cin>>N>>M;
+    cout<<N*M-1<<'\n';
+}
