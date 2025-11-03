17694fc60c7d652def1b6ade240d94d0e5125aaa
june.lee (이광준) <154116417+availrum@users.noreply.github.com>
2025-11-04 00:19:28 +0900

    Add files via upload

---
+++ stone_game.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){ //9655
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    if(N%2==0) cout<<"CY\n";
+    else cout<<"SK\n";
+} 
